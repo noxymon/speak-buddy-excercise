93fe42be463efd4b6b14a8b69c964a20874efc3f
noxymon <avicienna.ulhaq@gmail.com>
2024-11-15 08:51:24 +0700

    Add sprindoc as api-docs support

--- src/main/java/com/speakbuddy/service/soundreceiverapi/controller/AudioController.java
+++ src/main/java/com/speakbuddy/service/soundreceiverapi/controller/AudioController.java
@@ -7,6 +7,10 @@ import com.speakbuddy.service.soundreceiverapi.service.audio.ServiceException;
 import com.speakbuddy.service.soundreceiverapi.service.audio.parameter.AudioRetrieverInput;
 import com.speakbuddy.service.soundreceiverapi.service.audio.parameter.AudioRetrieverOutput;
 import com.speakbuddy.service.soundreceiverapi.service.audio.parameter.AudioSaverInput;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
@@ -25,7 +29,9 @@ public class AudioController {
     private final AudioSaver audioSaver;
     private final AudioRetriever audioRetriever;
 
-    @PostMapping("/user/{userId}/phrase/{phraseId}")
+    @Operation(summary = "Upload audio file to server", description = "it needs userId, PhraseId, and audio file through form uploaded multipart. This api also automatically convert the uploaded file to specific format.")
+    @ApiResponses(value = {@ApiResponse(description = "Success operation")})
+    @PostMapping(value = "/user/{userId}/phrase/{phraseId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity storeAudio(@PathVariable("userId") int userId, @PathVariable("phraseId") int phraseId,
                                      @RequestParam("audio_file") MultipartFile file) {
         if (file.isEmpty()) {
@@ -45,6 +51,13 @@ public class AudioController {
         return ResponseEntity.accepted().build();
     }
 
+    @Operation(summary = "Retrieve audio file by format", description = "Retrieve an audio file for a specific user and phrase in the specified format.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    description = "Success operation",
+                    content = {@Content(mediaType = "audio/wav")}
+            )
+    })
     @GetMapping("/user/{userId}/phrase/{phraseId}/{format}")
     public ResponseEntity getAudioByFormat(@PathVariable("userId") int userId, @PathVariable("phraseId") int phraseId,
                                            @PathVariable("format") String format){
